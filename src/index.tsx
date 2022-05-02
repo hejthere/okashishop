1c43fe574fb0f1deab5ac661487d87b4d1bb42f4
Connie <coke14190@gmail.com>
2022-05-02 23:10:05 +0800

    feat: add base name

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/okashishop">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
